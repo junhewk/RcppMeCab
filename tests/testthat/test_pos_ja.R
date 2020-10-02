6e0fcc54eec55b80d20e36526bf6ed7c24609e7c
Kato Akiru <am11.44@gmail.com>
2020-10-02 09:56:51 +0900

    fix test

--- tests/testthat/test_pos_ja.R
+++ tests/testthat/test_pos_ja.R
@@ -1,16 +1,10 @@
-if (Sys.getenv("MECAB_LANG") == "ja") {
-  test_that("Test pos tagger works", {
-    expect_equal(
-      pos(
-        enc2utf8("\u982d\u304c\u8d64\u3044\u9b5a\u3092\u98df\u3079\u305f\u732b"),
-        format = "data.frame"
-      )[4, 3],
-      enc2utf8("\u8d64\u3044")
-    )
-  })
-} else {
-  test_that("Skipped the test on Japanese", {
-    expect_true(TRUE)
-  })
-}
-
+test_that("Test pos tagger works", {
+  skip_if_not(Sys.getenv("MECAB_LANG") == "ja", "Skip testing on Japanese")
+  expect_equal(
+    pos(
+      enc2utf8("\u982d\u304c\u8d64\u3044\u9b5a\u3092\u98df\u3079\u305f\u732b"),
+      format = "data.frame"
+    )[4, 3],
+    enc2utf8("\u8d64\u3044")
+  )
+})
