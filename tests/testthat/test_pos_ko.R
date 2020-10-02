87edb076c59e68b5d99a00c24755a416291d0ec4
Kato Akiru <am11.44@gmail.com>
2020-10-02 10:51:23 +0900

    fix test

--- tests/testthat/test_pos_ko.R
+++ tests/testthat/test_pos_ko.R
@@ -4,7 +4,7 @@ test_that("Test pos tagger works on ko", {
     pos(
       enc2utf8("\uc548\ub155\ud558\uc138\uc694 \uc800\ub294 \ubc15\ucc2c\uc5fd \uc785\ub2c8\ub2e4."),
       format = "data.frame"
-    )[4, 3],
+    )[4, 4],
     enc2utf8("\uc138\uc694")
   )
 })
