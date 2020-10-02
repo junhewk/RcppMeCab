264c3148f3bac047e0728de5c035eff16fe4fbde
Kato Akiru <am11.44@gmail.com>
2020-10-02 11:06:45 +0900

    fix Rbuildignore

--- tests/testthat/test_pos_ja.R
+++ tests/testthat/test_pos_ja.R
@@ -5,6 +5,6 @@ test_that("Test pos tagger works on ja", {
       enc2utf8("\u982d\u304c\u8d64\u3044\u9b5a\u3092\u98df\u3079\u305f\u732b"),
       format = "data.frame"
     )[4, 4],
-    enc2utf8("\u8d64\u3044")
+    enc2utf8("\u9b5a")
   )
 })
--- tests/testthat/test_pos_ko.R
+++ tests/testthat/test_pos_ko.R
@@ -5,6 +5,6 @@ test_that("Test pos tagger works on ko", {
       enc2utf8("\uc548\ub155\ud558\uc138\uc694 \uc800\ub294 \ubc15\ucc2c\uc5fd \uc785\ub2c8\ub2e4."),
       format = "data.frame"
     )[4, 4],
-    enc2utf8("\uc138\uc694")
+    enc2utf8("\uc800")
   )
 })
