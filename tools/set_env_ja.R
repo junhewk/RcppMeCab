082f5915bc24b20db85451540293897babdff47f
Kato Akiru <am11.44@gmail.com>
2021-01-17 15:20:44 +0900

    add renv and tools dir (#1)

---
+++ tools/set_env_ja.R
@@ -0,0 +1,3 @@
+## For Japanese mecab
+Sys.setenv("MECAB_LANG" = "ja")
+options("mecabSysDic" = "C:/Program Files (x86)/MeCab/dic")
---
+++ tools/set_env_ko.R
@@ -0,0 +1,3 @@
+## For Korean mecab
+Sys.setenv("MECAB_LANG" = "ko")
+options("mecabSysDic" = "C:/MeCab-ko/mecab-ko-dic")
